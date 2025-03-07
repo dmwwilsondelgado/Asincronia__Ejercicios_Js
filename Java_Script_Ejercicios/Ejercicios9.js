24ee9c6b36271c3a11af9e11629f5a28b2eff343
✡ 𝓦𝓲𝓵𝓲 𝓓𝓮𝓵𝓰𝓪𝓭𝓸 ✡ <wilsondelgadomoreno@gmail.com>
2025-03-06 21:04:57 -0500

    Ejercicio Noveno

---
+++ Java_Script_Ejercicios/Ejercicios9.js
@@ -0,0 +1,19 @@
+// ¿Cuál es el resultado del código a continuación?
+
+
+let i =0 ; 
+
+
+setTimeout(() =>alert(i),100);
+
+
+// asumimos que el tioempoi de esta funcion es  > 100ms
+
+for (let j = 0; j < 10000; j++) {
+    i++;
+}
+
+
+
+
+alert(" El resultadfo de esta Funcio es 1000")
